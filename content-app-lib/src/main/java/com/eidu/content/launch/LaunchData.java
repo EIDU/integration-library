cbbf61f72bb9ea7c81a3393743b0ee9a1d2a3d87
Mathias Lenz <mlenz@small-improvements.com>
2021-08-05 10:23:33 +0200

    ✨ Content App Lib: add launch and result classes for content apps - E56

---
+++ content-app-lib/src/main/java/com/eidu/content/launch/LaunchData.java
@@ -0,0 +1,162 @@
+package com.eidu.content.launch;
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Arrays;
+
+public class LaunchData {
+
+    public static final String CONTENT_ID_EXTRA = "contentId";
+    public static final String WORK_UNIT_RUN_ID_EXTRA = "workUnitRunId";
+    public static final String LEARNER_ID_EXTRA = "learnerId";
+    public static final String SCHOOL_ID_EXTRA = "schoolId";
+    public static final String ENVIRONMENT_EXTRA = "environment";
+    public static final String REMAINING_FOREGROUND_TIME_EXTRA = "remainingForegroundTimeInMs";
+    public static final String INACTIVITY_TIMEOUT_EXTRA = "inactivityTimeoutInMs";
+
+    @NonNull
+    private final String contentId;
+    @NonNull
+    private final String workUnitRunId;
+    @NonNull
+    private final String learnerId;
+    @NonNull
+    private final String schoolId;
+    @NonNull
+    private final String environment;
+    @Nullable
+    private final Long remainingForegroundTimeInMs;
+    @Nullable
+    private final Long inactivityTimeoutInMs;
+
+    @NonNull
+    public static LaunchData fromPlainData(
+            @NonNull String contentId,
+            @NonNull String workUnitRunId,
+            @NonNull String learnerId,
+            @NonNull String schoolId,
+            @NonNull String environment,
+            @Nullable Long remainingForegroundTimeInMs,
+            @Nullable Long inactivityTimeoutInMs
+    ) {
+        return new LaunchData(
+                contentId,
+                workUnitRunId,
+                learnerId,
+                schoolId,
+                environment,
+                remainingForegroundTimeInMs,
+                inactivityTimeoutInMs
+        );
+    }
+
+    @NonNull
+    public static LaunchData fromLaunchIntent(@NonNull Intent intent) throws IllegalArgumentException {
+        String contentId = intent.getStringExtra(CONTENT_ID_EXTRA);
+        String workUnitRunId = intent.getStringExtra(WORK_UNIT_RUN_ID_EXTRA);
+        String learnerId = intent.getStringExtra(LEARNER_ID_EXTRA);
+        String schoolId = intent.getStringExtra(SCHOOL_ID_EXTRA);
+        String environment = intent.getStringExtra(ENVIRONMENT_EXTRA);
+        Long remainingForegroundTimeInMs = null;
+        if (intent.hasExtra(REMAINING_FOREGROUND_TIME_EXTRA)) {
+            remainingForegroundTimeInMs = intent.getLongExtra(REMAINING_FOREGROUND_TIME_EXTRA, 0);
+        }
+        Long inactivityTimeoutInMs = null;
+        if (intent.hasExtra(INACTIVITY_TIMEOUT_EXTRA)) {
+            remainingForegroundTimeInMs = intent.getLongExtra(INACTIVITY_TIMEOUT_EXTRA, 0);
+        }
+
+        for (String field : Arrays.asList(contentId, workUnitRunId, learnerId, schoolId, environment)) {
+            if (field == null || field.isEmpty()) {
+                throw new IllegalArgumentException(
+                        String.format("Invalid launch intent. A required field is missing. " +
+                                "[contentId: %s, workUnitRunId: %s, learnerId: %s, schoolId: %s, " +
+                                "environment: %s]", contentId, workUnitRunId, learnerId, schoolId, environment)
+                );
+            }
+        }
+
+        return new LaunchData(
+                contentId,
+                workUnitRunId,
+                learnerId,
+                schoolId,
+                environment,
+                remainingForegroundTimeInMs,
+                inactivityTimeoutInMs
+        );
+    }
+
+    public Intent toLaunchIntent(@NonNull String contentAppLaunchAction) {
+        Intent launchIntent = new Intent(contentAppLaunchAction)
+                .putExtra(CONTENT_ID_EXTRA, getContentId())
+                .putExtra(WORK_UNIT_RUN_ID_EXTRA, getWorkUnitRunId())
+                .putExtra(LEARNER_ID_EXTRA, getLearnerId())
+                .putExtra(SCHOOL_ID_EXTRA, getSchoolId())
+                .putExtra(ENVIRONMENT_EXTRA, getEnvironment());
+        if (getRemainingForegroundTimeInMs() != null) {
+            launchIntent.putExtra(REMAINING_FOREGROUND_TIME_EXTRA, remainingForegroundTimeInMs);
+        }
+        if (getInactivityTimeoutInMs() != null) {
+            launchIntent.putExtra(INACTIVITY_TIMEOUT_EXTRA, inactivityTimeoutInMs);
+        }
+        return launchIntent;
+
+    }
+
+    @NonNull
+    public String getContentId() {
+        return contentId;
+    }
+
+    @NonNull
+    public String getWorkUnitRunId() {
+        return workUnitRunId;
+    }
+
+    @NonNull
+    public String getLearnerId() {
+        return learnerId;
+    }
+
+    @NonNull
+    public String getSchoolId() {
+        return schoolId;
+    }
+
+    @NonNull
+    public String getEnvironment() {
+        return environment;
+    }
+
+    @Nullable
+    public Long getRemainingForegroundTimeInMs() {
+        return remainingForegroundTimeInMs;
+    }
+
+    @Nullable
+    public Long getInactivityTimeoutInMs() {
+        return inactivityTimeoutInMs;
+    }
+
+    private LaunchData(
+            @NonNull String contentId,
+            @NonNull String workUnitRunId,
+            @NonNull String learnerId,
+            @NonNull String schoolId,
+            @NonNull String environment,
+            @Nullable Long remainingForegroundTimeInMs,
+            @Nullable Long inactivityTimeoutInMs
+    ) {
+        this.contentId = contentId;
+        this.workUnitRunId = workUnitRunId;
+        this.learnerId = learnerId;
+        this.schoolId = schoolId;
+        this.environment = environment;
+        this.remainingForegroundTimeInMs = remainingForegroundTimeInMs;
+        this.inactivityTimeoutInMs = inactivityTimeoutInMs;
+    }
+}
---
+++ content-app-lib/src/main/java/com/eidu/content/result/LaunchResultData.java
@@ -0,0 +1,146 @@
+package com.eidu.content.result;
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LaunchResultData {
+
+    public static final String CONTENT_ID_EXTRA = "contentId";
+    public static final String RUN_WU_RESULT_EXTRA = "runWuResult";
+    public static final String SCORE_EXTRA = "score";
+    public static final String FOREGROUND_DURATION_EXTRA = "foregroundDurationInMs";
+    public static final String ADDITIONAL_DATA_EXTRA = "additionalData";
+
+    @NonNull
+    private final String contentId;
+    @NonNull
+    private final RunWuResult runWuResult;
+    @NonNull
+    private final Float score;
+    @NonNull
+    private final Long foregroundDurationInMs;
+    @NonNull
+    private final ArrayList<String> additionalData;
+
+    public static LaunchResultData fromPlainData(
+            @NonNull String contentId,
+            @NonNull RunWuResult runWuResult,
+            @NonNull Float score,
+            @NonNull Long foregroundDurationInMs,
+            @NonNull ArrayList<String> additionalData
+    ) {
+        return new LaunchResultData(
+                contentId,
+                runWuResult,
+                score,
+                foregroundDurationInMs,
+                additionalData
+        );
+    }
+
+    public static LaunchResultData fromResultIntent(@NonNull Intent resultIntent) {
+        String contentId = resultIntent.getStringExtra(CONTENT_ID_EXTRA);
+        RunWuResult runWuResult = RunWuResult.nullableValueOf(resultIntent.getStringExtra(RUN_WU_RESULT_EXTRA));
+        Float score = null;
+        if (resultIntent.hasExtra(SCORE_EXTRA)) {
+            score = resultIntent.getFloatExtra(SCORE_EXTRA, 0.f);
+        }
+        Long foregroundDurationInMs = null;
+        if (resultIntent.hasExtra(FOREGROUND_DURATION_EXTRA)) {
+            foregroundDurationInMs = resultIntent.getLongExtra(FOREGROUND_DURATION_EXTRA, 0);
+        }
+        ArrayList<String> additionalData = new ArrayList<>();
+        if (resultIntent.hasExtra(ADDITIONAL_DATA_EXTRA)) {
+            additionalData = resultIntent.getStringArrayListExtra(ADDITIONAL_DATA_EXTRA);
+        }
+
+        if (contentId == null || contentId.isEmpty() || runWuResult == null || score == null
+                || foregroundDurationInMs == null) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "Invalid result intent. A required field is missing. " +
+                                    "[contentId: %s, runWuResult: %s score: %f, foregroundDurationInMs: %d]",
+                            contentId, runWuResult, score, foregroundDurationInMs
+                    )
+            );
+        }
+
+        return new LaunchResultData(
+                contentId,
+                runWuResult,
+                score,
+                foregroundDurationInMs,
+                additionalData
+        );
+    }
+
+    public Intent toResultIntent() {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(CONTENT_ID_EXTRA, contentId);
+        resultIntent.putExtra(RUN_WU_RESULT_EXTRA, runWuResult.name());
+        resultIntent.putExtra(SCORE_EXTRA, score);
+        resultIntent.putExtra(FOREGROUND_DURATION_EXTRA, foregroundDurationInMs);
+        resultIntent.putStringArrayListExtra(ADDITIONAL_DATA_EXTRA, additionalData);
+        return resultIntent;
+    }
+
+    private LaunchResultData(
+            @NonNull String contentId,
+            @NonNull RunWuResult runWuResult,
+            @NonNull Float score,
+            @NonNull Long foregroundDurationInMs,
+            @NonNull ArrayList<String> additionalData
+    ) {
+        this.contentId = contentId;
+        this.runWuResult = runWuResult;
+        this.score = score;
+        this.foregroundDurationInMs = foregroundDurationInMs;
+        this.additionalData = new ArrayList<>(additionalData);
+    }
+
+    public enum RunWuResult {
+        Success,
+        Abort,
+        Error,
+        TimeoutInactivity,
+        TimeUp;
+
+        static RunWuResult nullableValueOf(String value) {
+            try {
+                return RunWuResult.valueOf(value);
+            } catch (IllegalArgumentException e) {
+                return null;
+            }
+        }
+    }
+
+    @NonNull
+    public String getContentId() {
+        return contentId;
+    }
+
+    @NonNull
+    public RunWuResult getRunWuResult() {
+        return runWuResult;
+    }
+
+    @NonNull
+    public Float getScore() {
+        return score;
+    }
+
+    @NonNull
+    public Long getForegroundDurationInMs() {
+        return foregroundDurationInMs;
+    }
+
+    @NonNull
+    public List<String> getAdditionalData() {
+        return Collections.unmodifiableList(additionalData);
+    }
+}
