8f3e39e4b7818588095af8bc45a92337d02fea44
Mathias Lenz <mlenz@small-improvements.com>
2021-08-13 14:35:23 +0200

    ✨ QueryIntent: use explicit intent for querying - E56

--- content-app-library/src/main/java/com/eidu/content/query/QueryIntent.java
+++ content-app-library/src/main/java/com/eidu/content/query/QueryIntent.java
@@ -5,13 +5,14 @@ import androidx.annotation.NonNull;
 
 public final class QueryIntent {
 
-    public static final String CATEGORY_QUERY_CONTENT_IDS =
+    public static final String ACTION_QUERY_CONTENT_IDS =
             "com.eidu.content.query.QUERY_CONTENT_IDS";
 
     @NonNull
-    public static Intent createIntent(@NonNull String queryAction) {
-        Intent intent = new Intent(queryAction);
-        intent.addCategory(CATEGORY_QUERY_CONTENT_IDS);
+    public static Intent createIntent(@NonNull String packageName, @NonNull String className) {
+        Intent intent = new Intent();
+        intent.setClassName(packageName, className);
+        intent.setAction(ACTION_QUERY_CONTENT_IDS);
         return intent;
     }
 
