14d16f75bfe1cfdf4d431ced5f5ee25cf24a2849
Mathias Lenz <mlenz@small-improvements.com>
2021-08-31 10:44:36 +0200

    ✅️ QueryIntentTest: add explicit intent test - while working on E56

--- content-app-library/src/androidTest/java/com/eidu/content/query/QueryIntentTest.java
+++ content-app-library/src/androidTest/java/com/eidu/content/query/QueryIntentTest.java
@@ -1,9 +1,11 @@
 package com.eidu.content.query;
 
+import static com.eidu.content.query.QueryIntent.ACTION_QUERY_CONTENT_IDS;
 import static com.eidu.content.query.QueryIntent.CATEGORY_QUERY_CONTENT_IDS;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import android.content.ComponentName;
 import android.content.Intent;
 
 import org.junit.jupiter.api.Test;
@@ -18,5 +20,17 @@ public class QueryIntentTest {
         Intent intent = QueryIntent.createIntent(queryAction);
         assertEquals(intent.getCategories(), Collections.singleton(CATEGORY_QUERY_CONTENT_IDS));
         assertEquals(intent.getAction(), queryAction);
+    }
+
+    @Test
+    public void createExplicitIntent() {
+        String packageName = "com.eidu.content";
+        String activity = "ContentActivity";
+        Intent intent = QueryIntent.createIntent(packageName, activity);
+        assertEquals(intent.getAction(), ACTION_QUERY_CONTENT_IDS);
+
+        ComponentName expected = new ComponentName(packageName, activity);
+
+        assertEquals(intent.getComponent(), expected);
     }
 }
