e0b54d3712f716187a9039b9d330f7a270e06332
Felix Engelhardt <felix.engelhardt@eidu.com>
2021-09-17 15:24:04 +0200

    ⬆️ Gradle: upgrade JUnit dependencies - while working on E88

--- content-app-library/build.gradle.kts
+++ content-app-library/build.gradle.kts
@@ -50,14 +50,14 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
 
     androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
     androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.2.2")
-    androidTestImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter-params:5.8.0")
     androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.2.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.0")
 
     testImplementation("androidx.test:core:1.4.0")
     testImplementation("org.mockito:mockito-core:3.12.4")
