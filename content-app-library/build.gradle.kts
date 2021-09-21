00dce0e403ccc55b0d86289955f6e8f5c0230bdc
tashonov <tashonov@gmail.com>
2021-09-21 13:34:34 +0200

    🔨 build.gradle.kts: update dependency, while at - T108

--- content-app-library/build.gradle.kts
+++ content-app-library/build.gradle.kts
@@ -51,9 +51,9 @@ dependencies {
 
     androidTestImplementation("androidx.test:runner:1.4.0")
     androidTestImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
-    androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.2.2")
+    androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.3.0")
     androidTestImplementation("org.junit.jupiter:junit-jupiter-params:5.8.0")
-    androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.2.2")
+    androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.3.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
