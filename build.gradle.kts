f02f6808ebde8933d7346c3b36b93876de01c40d
Mathias Lenz <mlenz@small-improvements.com>
2022-11-28 18:18:33 +0100

    ⬆ build.gradle.kts: update additional libraries - KOCHA-222

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.1")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
--- integration-library/build.gradle.kts
+++ integration-library/build.gradle.kts
@@ -50,19 +50,19 @@ android {
 }
 
 dependencies {
-    implementation("androidx.annotation:annotation:1.3.0")
+    implementation("androidx.annotation:annotation:1.5.0")
 
-    androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    androidTestImplementation("androidx.test:runner:1.5.1")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.3.0")
-    androidTestImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
     androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.3.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
 
-    testImplementation("androidx.test:core:1.4.0")
+    testImplementation("androidx.test:core:1.5.0")
     testImplementation("org.mockito:mockito-core:4.0.0")
 }
 
